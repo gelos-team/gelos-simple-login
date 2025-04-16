6c91b0fb9f27e3e1c7312b0282f45f82c67fd495
Mitch Coghlan <mitch.c37@outlook.com>
2025-04-16 12:08:35 +1000

    Improved the way to read from the database.

--- src/database.py
+++ src/database.py
@@ -16,31 +16,61 @@ class DatabaseReadError(Exception):
         super().__init__(*args)
 
 
+class Account:
+    def __init__(self, username: str, password: str) -> None:
+        self.username = username
+        self.password = password
+
+
 class DatabaseManager:
     def __init__(self, database_path: Path | str, break_upon_error: bool = False) -> None:
         # The location leading to the database
         self.path: Path = Path(database_path) if type(database_path) == str else database_path
-
 
         # Stop if an error occurred unless told otherwise
         self.break_upon_error = break_upon_error
 
     
     # Read from the database
-    def __read__(self, path: Path, break_upon_error: bool = False) -> list[dict]:
+    def __read__(self, path: Path, break_upon_error: bool = False) -> list[Account]:
         try:
             # Stop if the database doesn't exist.
-            if not os.path.exists(path):
-                # Print an error message if told too
-                if break_upon_error:
-                    raise FileNotFoundError("The database at " + str(path.resolve()) + " doesn't exist inside the computer.")
-        except Exception as err:
-            # Print an error message unless said otherwise.
-            if break_upon_error:
-                raise DatabaseReadError(str(type(err).__name__) + ": " + str(err))
+            if not path.exists(follow_symlinks=False):
+                # Print an error message saying that the database doesn't exist.
+                raise FileNotFoundError(f"The database could not be found at {path.resolve(True)}. Please check your spelling and try again.")
             
-            # Output nothing and exit
+                return []
+            
+
+            # Try and read from the database.
+            database_text: str = ""
+
+
+            with path.open() as database_file:
+                # Get the contents of the database, including the credentials of the accounts.
+                database_text = database_file.read().strip()
+
+
+            # Do nothing if the database is empty
+            if len(database_text) <= 0:
+                return []
+            
+
+            # Get the usernames and passwords from every account
+            account_list: list[Account] = []
+
+
+            for account in database_text.split("\n"):
+                account_list.append(Account(account.strip().split(",")[0], account.strip().split(",")[1]))
+
+            
+            return account_list
+        except Exception as err:
+            if break_upon_error:
+                raise DatabaseReadError(f"{type(err).__name__} - {err}")
+            
             return []
-        
-    def read(self) -> list[dict]:
+    
+
+    def read(self) -> list[Account]:
         return self.__read__(self.path, self.break_upon_error)
