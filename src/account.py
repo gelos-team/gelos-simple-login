b3c75701ffb9ff2021cb593e0e50e78065cb893b
Mitch Coghlan <mitch.c37@outlook.com>
2025-04-19 20:06:55 +1000

    19.04.2025 - Started work on the account registration process.

--- src/account.py
+++ src/account.py
@@ -4,5 +4,21 @@
 
 
 """
-    Purpose: <insert purpose here>
+    Purpose: Responsible for handling accounts inside the database
 """
+
+
+from pathlib import *
+from database import *
+
+
+class AccountManager:
+    def __init__(self, db_manager: DatabaseManager) -> None:
+        # The main database manager
+        self.db_manager: DatabaseManager = db_manager
+
+    # Handle the account registration process.
+    def register_account(self) -> None:
+        pass
+
+        
