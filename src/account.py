04443db1e3dbdacabba1ba68742f9d1a78cb1acd
Mitch Coghlan <mitch.c37@outlook.com>
2025-04-26 19:31:55 +1000

    Provided better error handling.

--- src/account.py
+++ src/account.py
@@ -13,12 +13,22 @@ from database import *
 from getpass import getpass
 import string
 
+
+class InvalidCredentialsError(Exception):
+    pass
+
+
+# Print an error message.
+def print_error(msg: object) -> None:
+    sys.stderr.write("ERROR: " + str(msg) + "\n")
+
+
 class AccountManager:
-    def __init__(self, db_manager: DatabaseManager) -> None:
+    def __init__(self, db_manager: DatabaseManager, break_upon_error: bool = False) -> None:
         # The main database manager
         self.db_manager: DatabaseManager = db_manager
+        self.break_upon_error: bool = break_upon_error
         self.current_account: str = ""
-
     # Handle the account registration process.
     def register_account(self) -> None:
         running: bool = True
@@ -56,8 +66,7 @@ class AccountManager:
 
                 # Check if the username isn't empty.
                 if len(username) <= 0:
-                    sys.stderr.write("ERROR: Please enter a username.\n")
-                    continue
+                    raise InvalidCredentialsError("ERROR: Please enter a username.\n")
 
 
                 # Check if the username is available.
@@ -71,8 +80,7 @@ class AccountManager:
                             break
 
                     if not username_available:
-                        sys.stderr.write("ERROR: The username " + username + " is not available. Please choose a different one and try again.\n")
-                        continue
+                        raise InvalidCredentialsError(f"The username {username} is not available. Please choose a different one and try again.")
 
 
                 # Now ask the user to enter a password for 
@@ -114,8 +122,7 @@ class AccountManager:
                 # haven't been met.
                 if not (has_lowercase_letters and has_uppercase_letters and \
                         has_numbers and has_symbols) or len(password) < 8:
-                    sys.stderr.write("ERROR: Password must have atleast 8 characters, lowercase letters, uppercase letters, numbers and symbols.\n")
-                    continue
+                    raise InvalidCredentialsError("Password must have atleast 8 characters, lowercase letters, uppercase letters, numbers and symbols.")
 
                 
                 # Check if the password is entered correctly.
@@ -123,8 +130,8 @@ class AccountManager:
 
 
                 if password != password_2:
-                    sys.stderr.write("ERROR: Passwords do not match. Please try again.\n")
-                    continue
+                    raise InvalidCredentialsError("Passwords do not match. Please try again.")
+                    
 
 
                 # Ask if the user wishes to create their account
@@ -145,10 +152,22 @@ class AccountManager:
                     else:
                         sys.stderr.write("ERROR: Please choose a valid option.")
 
+            except InvalidCredentialsError as err:
+                print_error(str(err))
+                continue
+
             except KeyboardInterrupt:
                 print("\nAccount creation cancelled")
                 running = False
-                break          
+                break
+
+            except Exception as err:
+                if self.break_upon_error: # Print an error message if specified.
+                    raise type(err)(str(err))
+                
+                # Otherwise, stop the login process
+                print_error("Something went wrong: " + str(err))
+                return
 
 
     # Handle the log in process.
@@ -196,8 +215,7 @@ class AccountManager:
                 
                 # Restart if there isn't a match.
                 if not username_matches:
-                    sys.stderr.write("ERROR: The username " + username + " doesn\'t exist. Please try again.\n")
-                    continue
+                    raise InvalidCredentialsError(f"The username {username} doesn\'t exist. Please try again.")
 
 
                 # Prompt the user to enter a password for the account.
@@ -220,8 +238,7 @@ class AccountManager:
 
                 # Restart if the password is incorrect
                 if not is_password_correct:
-                    sys.stderr.write("Password is incorrect. Please try again.\n")
-                    continue
+                    raise InvalidCredentialsError("Password is incorrect. Please try again.")
 
 
                 # Log in using the account details and exit.
@@ -231,54 +248,77 @@ class AccountManager:
                 running = False
                 break
                 
+            except InvalidCredentialsError as err:
+                print_error(str(err))
+                continue
+
 
             except KeyboardInterrupt:
                 print("\nLog in cancelled")
                 running = False
-                break  
-
+                break
+            except Exception as err:
+                if self.break_upon_error: # Print an error message if specified.
+                    raise type(err)(str(err))
+                
+                # Otherwise, stop the login process
+                print_error("Something went wrong: " + str(err))
+                return
+                
     # View list of accounts without their passwords.
     def view_list(self) -> None:
-        # Stop if either the database doesn't exist or is empty.
-        if not self.db_manager.path.exists or len(self.db_manager.read()) <= 0:
-            print("There is nothing there.")
+        try:
+            # Stop if either the database doesn't exist or is empty.
+            if not self.db_manager.path.exists or len(self.db_manager.read()) <= 0:
+                print("There is nothing there.")
+                return
+            
+
+            # Check if the user is logged in.
+            is_logged_in: bool = False
+
+            for account in self.db_manager.read().split("\n"):
+                # Get the username and check if there is a match.
+                if self.current_account == account.split(",")[0].strip():
+                    is_logged_in = True
+                    break
+
+            if not is_logged_in:
+                raise InvalidCredentialsError("You must log yourself in before viewing the list of users.")
+
+            
+            # Display a list of users without their passwords.
+            list_of_users: str = ""
+
+            index: int = 1 # The list index
+
+
+            for account in self.db_manager.read().strip().split("\n"):
+                try:
+                    # Get the username from the account
+                    username: str = account.split(",")[0].strip()
+
+                    # Now add the username to the list
+                    list_of_users += f"{index}: {username}\n"
+                except Exception as err: # Skip if an error had occurred
+                    continue
+
+                index += 1
+
+
+            print(list_of_users.strip())
+
+        except InvalidCredentialsError as err:
+            print_error(str(err))
             return
-        
 
-        # Check if the user is logged in.
-        is_logged_in: bool = False
-
-        for account in self.db_manager.read().split("\n"):
-            # Get the username and check if there is a match.
-            if self.current_account == account.split(",")[0].strip():
-                is_logged_in = True
-                break
-
-        if not is_logged_in:
-            sys.stderr.write("ERROR: You must log yourself in before viewing the list of users.")
+        except Exception as err:
+            if self.break_upon_error: # Print an error message if specified.
+                raise type(err)(str(err))
+            
+            # Otherwise, stop the login process
+            sys.stderr.write("Something went wrong: " + str(err))
             return
-
-        
-        # Display a list of users without their passwords.
-        list_of_users: str = ""
-
-        index: int = 1 # The list index
-
-
-        for account in self.db_manager.read().strip().split("\n"):
-            try:
-                # Get the username from the account
-                username: str = account.split(",")[0].strip()
-
-                # Now add the username to the list
-                list_of_users += f"{index}: {username}\n"
-            except Exception as err: # Skip if an error had occurred
-                continue
-
-            index += 1
-
-
-        print(list_of_users.strip())
 
                 
 
