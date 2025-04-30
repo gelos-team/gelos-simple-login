d8381acfd7f85731da454cb033fd91a2504c718c
Mitch Coghlan <mitch.c37@outlook.com>
2025-04-30 10:12:19 +1000

    Create the entrypoint for the program

--- src/main.py
+++ src/main.py
@@ -3,5 +3,31 @@
 
 
 """
-    Description: <insert purpose here>
+    Description: The main program
 """
+
+from user_interface import UserInterface
+from pathlib import Path
+
+
+# The location pointing to the database.
+database_path: Path = Path("./test_data/accounts.txt")
+
+
+class App:
+    # Setup everything before continuing
+    def __init__(self, path: Path, break_upon_error: bool = False) -> None:
+        # Settings
+        self.path = path
+        self.break_upon_error = break_upon_error
+
+    
+    def run(self) -> None:
+        # Create the user interface
+        ui: UserInterface = UserInterface(self.path, self.break_upon_error)
+        ui.run()
+
+
+if __name__ == "__main__":
+    app: App = App(database_path, True)
+    app.run()
