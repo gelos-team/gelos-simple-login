dd0c3aff4c5583d704de93c5bcd5c2e4162a6c54
cogos18 <>
2025-05-04 14:35:35 +0800

    Made improvements over the list of menu options

--- src/user_interface.py
+++ src/user_interface.py
@@ -90,6 +90,35 @@ class UserInterface:
         self.quit_command = quit_command
 
 
+        # The list of predefined options
+        self.predefined_options: list[dict] = [
+            {
+                "label": "Log in to your account",
+                "id": "login",
+                "requires_login": False,
+                "alias": "1",
+                "visible": True,
+                "command": self.account_manager.login
+            },
+            {
+                "label": "Create an account",
+                "id": "register",
+                "requires_login": False,
+                "alias": "2",
+                "visible": True,
+                "command": self.account_manager.register_account
+            },
+            {
+                "label": "View list of users",
+                "id": "view_list",
+                "requires_login": True,
+                "alias": "3",
+                "visible": True,
+                "command": self.account_manager.view_list
+            }
+        ]
+
+
     # Check if a menu option already exists.
     def menu_option_exists(self, id: str) -> bool:
         """
@@ -178,26 +207,24 @@ class UserInterface:
             The options include the option to log in, create accounts and view a list of users (when logged in).
         """
 
-        # Clear everything before continuing
-        self.clear_menu_options()
+
+        for option in self.predefined_options:
+            if type(option) is not dict: # Skip if an invalid option is detected
+                continue
+
+            # Add the option to the list
+            if option["requires_login"] and not self.account_manager.is_logged_in(): # Skip if not logged in unless said otherwise.
+                self.remove_menu_option(option["id"])
+            else:
+                self.add_menu_option(option["label"], option["id"], option["alias"], \
+                                     option["command"], self.predefined_options.index(option) + 1, \
+                                        option["visible"])
 
 
-        # Add the options to log in or sign up
-        self.add_menu_option("Log in", "login", "1", self.account_manager.login, 0) \
-            if not self.account_manager.db_manager.is_database_empty_or_nonexistent() \
-                else self.remove_menu_option("login")
-        
-
-        self.add_menu_option("Sign up", "register", "2", self.account_manager.register_account, 1)
-        
-        # Add the option to view a list of accounts when logged in.
-        self.add_menu_option("View list of users", "list_user_accounts", "3", self.account_manager.view_list, 2) \
-        if self.account_manager.is_logged_in() else \
-        self.remove_menu_option("list_user_accounts")
-
-
-        # Add the option to exit out of the program.
-        self.add_menu_option("Quit", "exit", "q", self.quit_command, 3)
+            # Always add an option to quit.
+            self.remove_menu_option("quit")
+            self.add_menu_option("Quit", "quit", "Q", self.quit_command, len(self.menu_options))
+            
 
 
     # Output the heading of the program.
